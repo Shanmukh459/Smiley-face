408f4be585f7f578e5ea6b9e927da72f3ca6df38
Shanmukh459 <saikrrish459@gmail.com>
2024-04-17 12:15:25 -0700

    recreated the simley face

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { arc } from 'd3';
 
-console.log(arc);
-const width = 960;
-const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 20;
-const eyeOffsetX = 110;
-const eyeOffsetY = 90;
-const eyeRadius = 40;
-const mouthWidth = 20;
-const mouthRadius = 130;
+const width = 960
+const height = 500
+const centerX = width / 2
+const centerY = height /2
+const eyeOffsetX = 100
+const eyeOffsetY = 80
+const strokeWidth = 10
+const eyeRadius = 30
+const mouthWidth = 20
+const mouthRadius = 140
+
 
 const mouthArc = arc()
   .innerRadius(mouthRadius)
-  .outerRadius(mouthRadius + mouthWidth)
-  .startAngle(Math.PI / 2)
-  .endAngle((Math.PI * 3) / 2);
+  .outerRadius(mouthRadius+mouthWidth)
+  .startAngle(Math.PI/2)
+  .endAngle(Math.PI * 3/2)
 
 const App = () => {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${centerX}, ${centerY})`}>
-        <circle
-          r={centerY - strokeWidth / 2}
+        <circle 
+          r={centerY - strokeWidth/2}
           fill="yellow"
           stroke="black"
-          stroke-width={strokeWidth}
+          strokeWidth={strokeWidth}
         />
-        <circle
+        <circle 
+          r={eyeRadius}
           cx={-eyeOffsetX}
           cy={-eyeOffsetY}
-          r={eyeRadius}
         />
-        <circle
+        <circle 
+          r={eyeRadius}
           cx={eyeOffsetX}
           cy={-eyeOffsetY}
-          r={eyeRadius}
         />
         <path d={mouthArc()} />
       </g>
     </svg>
-  );
-};
+  )
+}
 
 export default App
