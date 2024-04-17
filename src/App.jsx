ba95e7d8954020249db509203eae152bfee064e9
Shanmukh459 <saikrrish459@gmail.com>
2024-04-17 13:28:55 -0700

    creating multiple faces

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Face } from './Face'
+import { range } from 'd3';
 
-const width = 960
-const height = 500
+const width = 190
+const height = 190
+const array = range(10*5)
 
+const App = () => array.map(() => (
+  <Face 
+    width={width}
+    height={height}
+    centerX = {width / 2}
+    centerY = {height /2}
+    eyeOffsetX = {38}
+    eyeOffsetY = {35}
+    strokeWidth = {10}
+    eyeRadius = {13}
+    mouthWidth = {10}
+    mouthRadius = {45}
+    />
+  
+))
 
-const App = () => {
-  return <Face 
-  width={width}
-  height={height}
-  centerX = {width / 2}
-  centerY = {height /2}
-  eyeOffsetX = {100}
-  eyeOffsetY = {80}
-  strokeWidth = {10}
-  eyeRadius = {30}
-  mouthWidth = {20}
-  mouthRadius = {140}
-  />
-}
 
 export default App
